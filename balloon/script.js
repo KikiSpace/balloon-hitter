e926354be1fcec9c590258173b66d817dfd7e51d
KikiSpace <122402506+KikiSpace@users.noreply.github.com>
2023-01-10 23:30:06 -0800

    version1- onclick balloon reset

---
+++ balloon/script.js
@@ -0,0 +1,91 @@
+//SOLID principle
+const balloons = [];
+const hitBalloonAudio = new Audio();
+const gameCanvas = document.createElement('div');
+gameCanvas.classList.add('gameCanvas');
+
+const scoreBoard = document.createElement('div');
+scoreBoard.classList.add('scoreBoard');
+
+scoreBoard.innerText = '0';
+
+document.body.append(gameCanvas);
+document.body.append(scoreBoard);
+
+const gameCanvasHeight = gameCanvas.offsetHeight;
+const gameCanvasWidth = gameCanvas.offsetWidth;
+
+class Balloon {
+    constructor() {
+        this.balloonNode = document.createElement('div');
+        this.balloonNode.classList.add('balloon');
+
+        this.setRandomLeft();
+        this.setRandomColor();
+
+        this.speed = Math.ceil(Math.random() * 10);
+
+        this.balloonNode.style.animationDuration = `${30/this.speed}s`;
+
+        this.balloonNode.onclick = () => {
+            this.balloonNode.style.display = 'none';
+
+            this.setRandomLeft();
+            this.setRandomColor();
+
+            let currentScore = Number(scoreBoard.innerText);
+            currentScore += this.speed;
+
+            this.balloonNode.style.display = 'block';
+            scoreBoard.innerText = `${currentScore}`;
+        }
+
+    }
+
+    setRandomColor() {
+        this.balloonNode.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    }
+
+    setRandomLeft() {
+        this.balloonNode.style.left = `${Math.random() * gameCanvasWidth}px`;
+    }
+
+    setBottom(val) {
+        this.balloonNode.style.bottom = `${val}px`;
+    }
+
+    getCurrentBottom() {
+        return Number(this.balloonNode.style.bottom.split('px')[0]);
+    }
+
+    lift() {
+        const currentBottom = this.getCurrentBottom();
+        if(currentBottom >= gameCanvasHeight) {
+            this.setBottom(0);
+        } else {
+            this.setBottom(currentBottom + this.speed);
+        }
+    }
+}
+
+const balloonfragment = document.createDocumentFragment();
+for(let i = 0; i < 10; i ++) {
+    const balloon = new Balloon();
+    balloonfragment.append(balloon.balloonNode);
+    balloons.push(balloon);
+}
+
+gameCanvas.append(balloonfragment);
+
+function lifting() {
+    gameCanvas.style.display = 'none';
+    for(const balloon of balloons) {
+        balloon.lift();
+    }
+
+    gameCanvas.style.display = 'block';
+    requestAnimationFrame(lifting);
+}
+
+lifting();
+
